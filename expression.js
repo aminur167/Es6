515cd6dba8e28441438ea34eb72499c589a2a432
aminur167 <aminurhstu23@gmail.com>
2025-10-26 20:47:15 +0600

    es6

---
+++ expression.js
@@ -0,0 +1,13 @@
+let person = class
+{
+    constructor(name,age,profo)
+    {
+        this.name=name;
+        this.age=age;
+        this.profo=profo;
+    }
+}
+
+let newPerson=new person("Aminur Islam",18,"Web devloper");
+console.log(newPerson); 
+console.log(newPerson.age); 
