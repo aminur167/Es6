db28f238e207a89acff4dbb3aa3aafa577d7517d
aminur167 <aminurhstu23@gmail.com>
2025-10-27 10:36:06 +0600

    es6

---
+++ arrowFunction.js
@@ -0,0 +1,12 @@
+let fun=(x,y)=>x+y;
+console.log(fun(50,30));
+
+let fun1=(x,y)=>x-y;
+console.log(fun1(50,30));
+
+let fun2=(x,y)=>x*y;
+console.log(fun2(50,30));
+
+let fun3=(x,y)=>x/y;
+console.log(fun3(50,30));
+
