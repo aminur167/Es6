1f177e986594c0472f6d4d4183abf5741680c960
aminur167 <aminurhstu23@gmail.com>
2025-10-28 13:33:13 +0600

    es6

---
+++ Try.js
@@ -0,0 +1,15 @@
+try 
+{
+    console.log("Hello Aminur");
+    console.log(num);
+}
+
+catch (HabluError)
+{
+    console.log("How are you?");
+}
+
+finally
+{
+    console.log("Hello Webdevloper");
+}
